7d889823fec75a76c3dfed1e830f666cd03a1c50
Ata <atwarasulov@gmail.com>
2024-07-01 00:14:30 +0400

    fixes

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,5 +1,5 @@
 import { InputField } from '../components/InputField/InputField'
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 import css from './Layout.module.scss'
 import { Todo } from '@components/Todo/Todo'
 import { TodoTypes } from 'types/_types'
@@ -7,7 +7,6 @@ import Draggable from 'react-draggable'
 
 export const Layout = () => {
    const [todos, setTodos] = useState<TodoTypes[]>(JSON.parse(localStorage.getItem('todos') || '[]'))
-   const nodeRef = useRef(null)
 
    useEffect(() => {
       localStorage.setItem('todos', JSON.stringify(todos))
@@ -25,10 +24,11 @@ export const Layout = () => {
          <InputField setTodos={setTodos} todos={todos} />
          {todos.map((todo, index) => (
             <Draggable
+               cancel=".button"
                onStop={(_, data) => updatePos(data, index)}
                defaultPosition={todo.defaultPosition}
                key={todo.id}>
-               <div ref={nodeRef}>
+               <div>
                   <Todo setTodos={setTodos} todo={todo} todos={todos} />
                </div>
             </Draggable>
