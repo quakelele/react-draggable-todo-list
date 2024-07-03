5d6dab11688f6a9b996f7cf46cffaae40eef0f71
Ata <atwarasulov@gmail.com>
2024-07-03 14:01:16 +0400

    deleted some hooks

--- src/hooks/useToggler.ts
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react'
-
-export const useToggler = (initialValue: boolean) => {
-   const [value, setValue] = useState(initialValue)
-   const toggle = () => {
-      setValue(!initialValue)
-   }
-   return [value, toggle]
-}
